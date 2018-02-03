7eec7fc950291473f078dc60b69a932d6cf3d6a5
student37 <student37@S7N-PROGLANG-31.pcoip.ki.agh.edu.pl>
2018-02-03 14:51:17 +0100

    Added implementation of Invoice

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,30 +1,80 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Collection;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
 	private Collection<Product> products;
+	private BigDecimal subTotal;
+	private BigDecimal total;
+	private BigDecimal tax;
+
+	// konstruktor
+	Invoice() {
+		subTotal = new BigDecimal("0");
+		total = new BigDecimal("0");
+		tax = new BigDecimal("0");
+		products = new ArrayList<Product>();
+
+	}
 
 	public void addProduct(Product product) {
-		// TODO: implement
+		products.add(product);
+		update();
 	}
 
 	public void addProduct(Product product, Integer quantity) {
-		// TODO: implement
+		if (quantity <= 0) {
+			throw new IllegalArgumentException();
+		}
+		for (int i = 0; i < quantity; i++) {
+			products.add(product);
+		}
+		update();
+	}
+
+	public void setSubtotal() {
+		subTotal = BigDecimal.ZERO;
+		for (Product product : products) {
+			subTotal = subTotal.add(product.getPrice());
+		}
 	}
 
 	public BigDecimal getSubtotal() {
-		return null;
+		return subTotal;
+	}
+
+	public void setTax() {
+		tax = total.subtract(subTotal);
 	}
 
 	public BigDecimal getTax() {
-		return null;
+		return tax;
+	}
+
+	public void setTotal() {
+		total = BigDecimal.ZERO;
+		for (Product product : products) {
+			total = total.add(product.getPriceWithTax());
+		}
 	}
 
 	public BigDecimal getTotal() {
-		return null;
+		return total;
 	}
+
+	public static Invoice create() {
+		return new Invoice();
+	}
+
+	public void update() {
+		setSubtotal();
+		setTotal();
+		setTax();
+	}
+
 }
